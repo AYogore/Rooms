297ec412e0cf523bd81d5cc143bf7ac300149404
AYogore <AYogore@users.noreply.github.com>
2020-02-07 09:41:26 -0600

    Textbox inventory ui stuff

--- RoomsProject/Assets/Scripts/ItemScripts/Key.cs
+++ RoomsProject/Assets/Scripts/ItemScripts/Key.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Key : MonoBehaviour, IInventoryItem
 {
+    public GameObject Unique;
+    public GameObject TextBox;
+    public static bool GameIsPaused = false;
+
     public bool IsQuestItem
     {
         get
@@ -33,6 +37,36 @@ public class Key : MonoBehaviour, IInventoryItem
     {
         // Add logic what happens when key is picked up by player for example unlock door
         gameObject.SetActive(false);
+        Unique.SetActive(true); //key button active in inventory
+
+
+        //Pause(); //force open key text box FIX, RESUME DOES NOT WORK
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            if (GameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        TextBox.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        TextBox.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 
 }
